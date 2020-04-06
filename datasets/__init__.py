b965a5c2219e12d5dcf49df3757574acf7206441
vltanh <vltheanh030698@gmail.com>
2020-04-07 01:00:18 +0700

    minor changes

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,4 @@
 from .reidentification.siamese import SiameseDataset
 from .reidentification.triplet import TripletDataset
 from .aic2020track2 import AIC2020Track2, AIC2020Track2Hard
+from .image_folder import ImageFolderDataset
