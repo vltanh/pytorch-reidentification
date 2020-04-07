032ab03b6520f847b1789f588d1bfd16dae1abf2
vltanh <vltheanh030698@gmail.com>
2020-04-08 06:15:56 +0700

    add re-ranking

--- query.py
+++ query.py
@@ -24,27 +24,116 @@ def pdist_torch(emb1, emb2):
     return dist_mtx
 
 
-def create_submission(model_id, save_dir,
+def re_ranking(probFea, galFea, k1, k2, lambda_value, local_distmat=None, only_local=False):
+    # if feature vector is numpy, you should use 'torch.tensor' transform it to tensor
+    query_num = probFea.size(0)
+    all_num = query_num + galFea.size(0)
+    if only_local:
+        original_dist = local_distmat
+    else:
+        feat = torch.cat([probFea, galFea])
+        print('using GPU to compute original distance')
+        distmat = torch.pow(feat, 2).sum(dim=1, keepdim=True).expand(all_num, all_num) + \
+            torch.pow(feat, 2).sum(dim=1, keepdim=True).expand(
+                all_num, all_num).t()
+        distmat.addmm_(1, -2, feat, feat.t())
+        original_dist = distmat.cpu().numpy()
+        del feat
+        if not local_distmat is None:
+            original_dist = original_dist + local_distmat
+    gallery_num = original_dist.shape[0]
+    original_dist = np.transpose(original_dist / np.max(original_dist, axis=0))
+    V = np.zeros_like(original_dist).astype(np.float16)
+    initial_rank = np.argsort(original_dist).astype(np.int32)
+
+    print('starting re_ranking')
+    for i in range(all_num):
+        # k-reciprocal neighbors
+        forward_k_neigh_index = initial_rank[i, :k1 + 1]
+        backward_k_neigh_index = initial_rank[forward_k_neigh_index, :k1 + 1]
+        fi = np.where(backward_k_neigh_index == i)[0]
+        k_reciprocal_index = forward_k_neigh_index[fi]
+        k_reciprocal_expansion_index = k_reciprocal_index
+        for j in range(len(k_reciprocal_index)):
+            candidate = k_reciprocal_index[j]
+            candidate_forward_k_neigh_index = initial_rank[candidate, :int(
+                np.around(k1 / 2)) + 1]
+            candidate_backward_k_neigh_index = initial_rank[candidate_forward_k_neigh_index,
+                                                            :int(np.around(k1 / 2)) + 1]
+            fi_candidate = np.where(
+                candidate_backward_k_neigh_index == candidate)[0]
+            candidate_k_reciprocal_index = candidate_forward_k_neigh_index[fi_candidate]
+            if len(np.intersect1d(candidate_k_reciprocal_index, k_reciprocal_index)) > 2 / 3 * len(
+                    candidate_k_reciprocal_index):
+                k_reciprocal_expansion_index = np.append(
+                    k_reciprocal_expansion_index, candidate_k_reciprocal_index)
+
+        k_reciprocal_expansion_index = np.unique(k_reciprocal_expansion_index)
+        weight = np.exp(-original_dist[i, k_reciprocal_expansion_index])
+        V[i, k_reciprocal_expansion_index] = weight / np.sum(weight)
+    original_dist = original_dist[:query_num, ]
+    if k2 != 1:
+        V_qe = np.zeros_like(V, dtype=np.float16)
+        for i in range(all_num):
+            V_qe[i, :] = np.mean(V[initial_rank[i, :k2], :], axis=0)
+        V = V_qe
+        del V_qe
+    del initial_rank
+    invIndex = []
+    for i in range(gallery_num):
+        invIndex.append(np.where(V[:, i] != 0)[0])
+
+    jaccard_dist = np.zeros_like(original_dist, dtype=np.float16)
+
+    for i in range(query_num):
+        temp_min = np.zeros(shape=[1, gallery_num], dtype=np.float16)
+        indNonZero = np.where(V[i, :] != 0)[0]
+        indImages = [invIndex[ind] for ind in indNonZero]
+        for j in range(len(indNonZero)):
+            temp_min[0, indImages[j]] = temp_min[0, indImages[j]] + np.minimum(V[i, indNonZero[j]],
+                                                                               V[indImages[j], indNonZero[j]])
+        jaccard_dist[i] = 1 - temp_min / (2 - temp_min)
+
+    final_dist = jaccard_dist * (1 - lambda_value) + \
+        original_dist * lambda_value
+    del original_dist
+    del V
+    del jaccard_dist
+    final_dist = final_dist[:query_num, query_num:]
+    return final_dist
+
+
+def create_submission(model_id, save_root,
                       emb_query, names_query,
                       emb_gallery, names_gallery,
                       top_k=100):
 
-    save_dir = f'{save_dir}/{model_id}'
+    save_dir = f'{save_root}/{model_id}/OrgTool'
+    submit_path = f'{save_root}/{model_id}/{model_id}-track2.txt'
     os.makedirs(save_dir, exist_ok=True)
 
-    dist_mtx = pdist_torch(emb_query, emb_gallery).cpu().numpy()
+    # dist_mtx = pdist_torch(emb_query, emb_gallery).cpu().numpy()
+    dist_mtx = re_ranking(emb_query, emb_gallery, 55, 6, 0.1)
     n_q, n_g = dist_mtx.shape
     indices = np.argsort(dist_mtx, axis=1)
 
+    submit = [[] for _ in range(n_q)]
     for qidx in tqdm(range(n_q)):
         qimid = os.path.basename(names_query[qidx]).replace('.jpg', '')
-        save_file = save_dir + '/' + qimid + '.txt'
 
         match_id = names_gallery[indices[qidx, :top_k]]
+        submit[int(qimid) - 1] = [os.path.basename(x).replace('.jpg', '')
+                                  for x in match_id]
+
+    for qimid, result in enumerate(submit):
+        save_file = f'{save_dir}/{qimid + 1:06d}.txt'
         with open(save_file, 'w') as f:
             writer = csv.writer(f)
-            writer.writerows([[os.path.basename(x).replace('.jpg', '')]
-                              for x in match_id])
+            writer.writerows([[x] for x in result])
+
+    with open(submit_path, 'w') as g:
+        writer = csv.writer(g, delimiter=' ')
+        writer.writerows(submit)
 
 
 @torch.no_grad()
@@ -86,14 +175,14 @@ def generate_submission(gpus, weight_path, save_dir,
     ])
 
     print('Load queries...')
-    dataset = ImageFolderDataset('data/AIC20_ReID/image_query', transform)
+    dataset = ImageFolderDataset(query_dir, transform)
     dataloader = DataLoader(dataset, batch_size=64)
 
     print('Extract queries...')
     q_embs, q_ids = extract_embeddings(dataloader, net, device)
 
     print('Load gallery...')
-    dataset = ImageFolderDataset('data/AIC20_ReID/image_test', transform)
+    dataset = ImageFolderDataset(gallery_dir, transform)
     dataloader = DataLoader(dataset, batch_size=64)
 
     print('Extract gallery...')
