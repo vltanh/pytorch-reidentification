5c221a3b44023e95918979681cd07696afa71e4c
vltanh <vltheanh030698@gmail.com>
2020-04-09 16:39:16 +0700

    add ad-hoc classification loss

--- losses/reidentification/triplet_loss.py
+++ losses/reidentification/triplet_loss.py
@@ -50,3 +50,37 @@ class OnlineTripletLoss(nn.Module):
         losses = F.relu(ap_distances - an_distances + self.margin)
 
         return losses.mean()  # , len(triplets)
+
+
+class OnlineTripletWithClsLoss(nn.Module):
+    """
+    Online Triplets loss
+    Takes a batch of embeddings and corresponding labels.
+    Triplets are generated using triplet_selector object that take embeddings and targets and return indices of
+    triplets
+    """
+
+    def __init__(self, margin, selector):
+        super().__init__()
+        self.margin = margin
+        self.triplet_selector = getter.get_instance(selector)
+        self.cls = nn.Linear(1280, 333)
+
+    def forward(self, embeddings, target):
+
+        triplets = self.triplet_selector.get_triplets(embeddings, target)
+
+        if embeddings.is_cuda:
+            triplets = triplets.cuda()
+
+        ap_distances = (embeddings[triplets[:, 0]]
+                        - embeddings[triplets[:, 1]]).pow(2).sum(1)  # .pow(.5)
+        an_distances = (embeddings[triplets[:, 0]]
+                        - embeddings[triplets[:, 2]]).pow(2).sum(1)  # .pow(.5)
+        triplet_loss = F.relu(ap_distances - an_distances + self.margin)
+
+        classification = self.cls(embeddings)
+        cls_loss = F.cross_entropy(classification, target)
+
+        losses = triplet_loss + cls_loss
+        return losses.mean()  # , len(triplets)
